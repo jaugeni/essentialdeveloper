d844b4c048e7bb42d0af6e3a947e5866d17ebdef
Yauheni <jaugeni@gmail.com>
2023-07-11 17:35:00 -0600

    some small random changes

--- Playgrounds/SingletonsGlobalInstances.playground/Contents.swift
+++ Playgrounds/SingletonsGlobalInstances.playground/Contents.swift
@@ -16,33 +16,85 @@ enum CustomError: Error {
     case invalidResponse(_ response: URLResponse?)
 }
 
-class ApiClient {
-    static let share = ApiClient()
-    private init() {}
+class UserViewController: UIViewController {
+    var userInfo: ((([User]) -> Void) -> Void)!
     
-    func execute<T: Decodable>(url: String) async throws -> T {
-        guard let url = URL(string: url) else {
-            throw CustomError.invalideURL
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            throw CustomError.invalidResponse(response)
-        }
-        do {
-            return try JSONDecoder().decode(T.self, from: data)
-        } catch let error {
-            throw CustomError.invalidData(error)
+    convenience init(userInfo: @escaping (([User]) -> Void) -> Void) {
+        self.init()
+        self.userInfo = userInfo
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userInfo { userItem in
+            print(userItem)
         }
     }
     
 }
-func getUser(userName: String) async throws -> User {
-    try await ApiClient.share.execute(url: "https://api.github.com/users/\(userName)")
-}
 
+//class ApiClient {
+//    static let share = ApiClient()
+//    private init() {}
+//
+//    func execute<T: Decodable>(url: String) async throws -> T {
+//        guard let url = URL(string: url) else {
+//            throw CustomError.invalideURL
+//        }
+//
+//        let (data, response) = try await URLSession.shared.data(from: url)
+//
+//        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+//            throw CustomError.invalidResponse(response)
+//        }
+//        do {
+//            return try JSONDecoder().decode(T.self, from: data)
+//        } catch let error {
+//            throw CustomError.invalidData(error)
+//        }
+//    }
+//
+//}
 
-let data = try await getUser(userName: "jaugeni")
-print(data)
-PlaygroundPage.current.finishExecution()
+//class ApiClient {
+//    static let share = ApiClient()
+//    private init() {}
+//
+//    func execute<T: Decodable>(url: String) async throws -> T {
+//        guard let url = URL(string: url) else {
+//            throw CustomError.invalideURL
+//        }
+//
+//        let (data, response) = try await URLSession.shared.data(from: url)
+//
+//        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+//            throw CustomError.invalidResponse(response)
+//        }
+//        do {
+//            return try JSONDecoder().decode(T.self, from: data)
+//        } catch let error {
+//            throw CustomError.invalidData(error)
+//        }
+//    }
+//
+//}
+//
+//
+//func getUser(userName: String) async throws -> User {
+//    try await ApiClient.share.execute(url: "https://api.github.com/users/\(userName)")
+//}
+//
+//class UserInfo {
+//    var loadUser: (([User]) -> Void)
+//
+//    init(loadUser: @escaping (([User]) -> Void)) {
+//        self.loadUser = loadUser
+//    }
+//}
+//
+//let userInfo = UserInfo(loadUser: <#T##(([User]) -> Void)##(([User]) -> Void)##([User]) -> Void#>)
+//
+//
+//let data = try await getUser(userName: "jaugeni")
+//print(data)
+//PlaygroundPage.current.finishExecution()
