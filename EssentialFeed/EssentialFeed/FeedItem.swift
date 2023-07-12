39357d788fac925a2001bb5c7749feb1f7d8337f
Yauheni <jaugeni@gmail.com>
2023-07-11 19:01:55 -0600

    Feed Item  module

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Yauheni Ivaniuk on 7/11/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
