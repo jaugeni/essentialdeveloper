aa382ecf1a2240ee91df6bd6549ff92746b3550a
Yauheni <jaugeni@gmail.com>
2023-07-17 20:54:08 -0600

    refactored

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -57,7 +57,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
                 sut,
                 toCompleteWith: .failure(.invalidData),
                 when: {
-                    client.complete(withStatusCode: code, at: index)
+                    let json = makeItemsJSON([])
+                    client.complete(withStatusCode: code, data: json, at: index)
                 }
             )
         }
@@ -70,7 +71,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
             sut,
             toCompleteWith: .failure(.invalidData),
             when: {
-                let invalidJSON = Data(bytes: "ivalid json".utf8)
+                let invalidJSON = makeItemsJSON([["fake": "invalid JSON"]])
                 client.complete(withStatusCode: 200, data: invalidJSON)
             }
         )
@@ -184,7 +185,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
