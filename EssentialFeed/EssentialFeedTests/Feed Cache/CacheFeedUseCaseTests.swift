dac9cd87e551631803d9cd002f71f562459d4dd7
Yauheni <jaugeni@gmail.com>
2023-09-06 21:04:08 -0600

    save requests Cache Deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deleteCachedFeedCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -22,7 +33,30 @@ final class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreating() {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
+        
         XCTAssertEqual(store.deleteCachedFeedCount, 0)
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        XCTAssertEqual(store.deleteCachedFeedCount, 1)
+    }
+    
+    // MARK: - Helpers
 
+    private func uniqueItem() -> FeedItem {
+        FeedItem(
+            id: UUID(),
+            description: "any",
+            location: "any",
+            imageUrl: anyURL()
+        )
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
+    }
 }
