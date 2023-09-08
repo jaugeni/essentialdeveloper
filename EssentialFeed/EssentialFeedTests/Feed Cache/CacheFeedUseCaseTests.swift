bf3945359edca327ce119483db3be8a44856f162
Yauheni <jaugeni@gmail.com>
2023-09-07 21:15:18 -0600

    save_succeedsOnSuccesfullCacheInsertion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -63,6 +63,10 @@ class FeedStore {
     func completeInsertion (with error: Error, at index: Int = 0) {
         insertionCompleations[index](error)
     }
+    
+    func completeInsertionSuccesfully(at index: Int = 0) {
+        insertionCompleations[index](nil)
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -136,6 +140,24 @@ final class CacheFeedUseCaseTests: XCTestCase {
         
         XCTAssertEqual(recivedError as NSError?, insertionError)
     }
+    
+    func test_save_succeedsOnSuccesfullCacheInsertion() {
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
+        let exp = expectation(description: "Wait for save completion")
+        var recivedError: Error?
+        sut.save(items) { error in
+            recivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccesfully()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(recivedError)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
