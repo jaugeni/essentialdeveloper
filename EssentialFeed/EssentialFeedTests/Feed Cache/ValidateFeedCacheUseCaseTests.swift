3542b53575f5ca2bc1d40cd4391dc701c1a0daeb
Yauheni <jaugeni@gmail.com>
2023-09-27 20:34:59 -0600

    test_init_doesNotMessageStoreUponCreating

--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -5,4 +5,24 @@
 //  Created by Yauheni Ivaniuk on 9/27/23.
 //
 
-import Foundation
+import XCTest
+import EssentialFeed
+
+class ValidateFeedCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotMessageStoreUponCreating() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.recievedMessages, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
