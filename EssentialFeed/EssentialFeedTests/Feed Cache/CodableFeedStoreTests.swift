217e269a9fd254f021fc40a9bb8330b728d6153c
Yauheni <jaugeni@gmail.com>
2023-10-19 20:14:28 -0600

    moved insert code to reusable method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -93,12 +93,7 @@ class CodableFeedStoreTests: XCTestCase {
         let feed = uniqueImageFeed().local
         let timestamp = Date()
         
-        let exp = expectation(description: "Wait for cache retrieval")
-        sut.insert(feed, timestamp: timestamp) { insertionError in
-            XCTAssertNil(insertionError, "Expected feed to be inserted succesfully")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        insert((feed, timestamp), to: sut)
         
         expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
     }
@@ -108,12 +103,7 @@ class CodableFeedStoreTests: XCTestCase {
         let feed = uniqueImageFeed().local
         let timestamp = Date()
         
-        let exp = expectation(description: "Wait for cache inserted")
-        sut.insert(feed, timestamp: timestamp) { insertionError in
-            XCTAssertNil(insertionError, "Expected feed to be inserted succesfully")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        insert((feed, timestamp), to: sut)
         
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
     }
@@ -159,6 +149,16 @@ class CodableFeedStoreTests: XCTestCase {
             exp.fulfill()
         }
         
+        wait(for: [exp], timeout: 1.0)
+    }
+    private func insert(
+        _ cache: (feed: [LocalFeedImage], timestemp: Date),
+        to sut: CodableFeedStore) {
+        let exp = expectation(description: "Wait for cache inserted")
+            sut.insert(cache.feed, timestamp: cache.timestemp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted succesfully")
+            exp.fulfill()
+        }
         wait(for: [exp], timeout: 1.0)
     }
     
