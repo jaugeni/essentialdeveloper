4cfcdbb8cce77fbaea0dce3be84b8f97010c86a3
Yauheni <jaugeni@gmail.com>
2023-12-12 14:09:15 -0700

    EssentialFeedCacheIntegrationTests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Yauheni Ivaniuk on 12/12/23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+
+}
