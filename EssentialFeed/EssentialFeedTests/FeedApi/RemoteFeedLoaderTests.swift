2c161f40729c4ee02cf37982bf5537bcc3409cce
Yauheni <jaugeni@gmail.com>
2023-12-12 17:10:06 -0700

    updated HTTPClient using with standart Resul type

--- EssentialFeed/EssentialFeedTests/FeedApi/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedApi/RemoteFeedLoaderTests.swift
@@ -205,14 +205,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
 
         
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
         
@@ -227,7 +227,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
                 httpVersion: nil,
                 headerFields: nil
             )!
-            messages[index].completion(.success(data, response))
+            messages[index].completion(.success((data, response)))
         }
     }
 }
