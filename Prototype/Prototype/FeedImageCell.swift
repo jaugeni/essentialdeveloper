65326c2e4680f09e0640dd1dc210827c482a60c0
Yauheni <jaugeni@gmail.com>
2023-12-12 21:09:23 -0700

    FeedImageCell

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Yauheni Ivaniuk on 12/12/23.
+//
+
+import UIKit
+
+class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageContainer: UIView!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+}
