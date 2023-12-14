cb2ef200ebdf8af4ec7262ba96b4eff0b0563023
Yauheni <jaugeni@gmail.com>
2023-12-14 09:39:08 -0700

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EsseintialFeedIOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EsseintialFeedIOS
+//
+//  Created by Yauheni Ivaniuk on 12/14/23.
+//
+
+import UIKit
+
+ public final class FeedImageCell: UITableViewCell {
+     public let locationContainer = UIView()
+     public let locationLabel = UILabel()
+     public let descriptionLabel = UILabel()
+ }
+
