daa8addef024cc5f9d6e258111c667d1c941ec3c
Yauheni <jaugeni@gmail.com>
2023-12-15 09:10:27 -0700

    Add Presentable View Models as pure data to clarify communication between Presentation and UI.

--- EssentialFeed/EsseintialFeedIOS/Feed UI/FeedPresentation/FeedViewModel.swift
+++ EssentialFeed/EsseintialFeedIOS/Feed UI/FeedPresentation/FeedViewModel.swift
@@ -10,24 +10,24 @@ import EssentialFeed
 
 // MVVM example check MVP FeedPresenter
 
-final class FeedViewModel {
-    typealias Observer<T> = (T) -> Void
-    private let feedLoader: FeedLoader
-        
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    var onLoadingStateChange: Observer<Bool>?
-    var onFeedLoad: Observer<[FeedImage]>?
-    
-    func loadFeed() {
-        onLoadingStateChange?(true)
-        feedLoader.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.onFeedLoad?(feed)
-            }
-            self?.onLoadingStateChange?(false)
-        }
-    }
-}
+//final class FeedViewModel {
+//    typealias Observer<T> = (T) -> Void
+//    private let feedLoader: FeedLoader
+//        
+//    init(feedLoader: FeedLoader) {
+//        self.feedLoader = feedLoader
+//    }
+//    
+//    var onLoadingStateChange: Observer<Bool>?
+//    var onFeedLoad: Observer<[FeedImage]>?
+//    
+//    func loadFeed() {
+//        onLoadingStateChange?(true)
+//        feedLoader.load { [weak self] result in
+//            if let feed = try? result.get() {
+//                self?.onFeedLoad?(feed)
+//            }
+//            self?.onLoadingStateChange?(false)
+//        }
+//    }
+//}
