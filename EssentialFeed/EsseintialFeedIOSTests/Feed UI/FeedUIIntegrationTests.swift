6539dada017a4a7f6e6b0ea9677bc88b5c4eddfd
Yauheni <jaugeni@gmail.com>
2023-12-18 17:25:07 -0700

    Dispatch background feed image data completion to main thread before passing result to the UI components. Threading is dealt with a Decorator in the Composition layer.

--- EssentialFeed/EsseintialFeedIOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeed/EsseintialFeedIOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -18,7 +18,7 @@ final class FeedUIIntegrationTests: XCTestCase {
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(sut.title, localized("FEED_VIEW_TITLE"))
-     }
+    }
     
     func test_loadFeedActions_requestFeedFromLoader() {
         let (sut, loader) = makeSUT()
@@ -259,16 +259,31 @@ final class FeedUIIntegrationTests: XCTestCase {
     }
     
     func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
-         let (sut, loader) = makeSUT()
-         sut.loadViewIfNeeded()
-
-         let exp = expectation(description: "Wait for background queue")
-         DispatchQueue.global().async {
-             loader.completeFeedLoading(at: 0)
-             exp.fulfill()
-         }
-         wait(for: [exp], timeout: 1.0)
-     }
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        
+        let exp = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.completeFeedLoading(at: 0)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_loadImageDataCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [makeImage()])
+        _ = sut.simulateFeedImageViewVisible(at: 0)
+        
+        let exp = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.completeImageLoading(with: self.anyImageData(), at: 0)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
     
     // MARK: - Helpers
     
